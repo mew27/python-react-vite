61708d16a8551303b6421ef1628bf302cd9b9ab3
mew27 <terzolopaolo@libero.it>
2024-10-10 09:18:20 +0200

    Added requirements and preinstall script

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR. Hot module reload rules
+          Edit <code>src/App.tsx</code> and save to test HMR. HMR is cool
         </p>
       </div>
       <p className="read-the-docs">
